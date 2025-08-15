ba1539e0f80ef298627db0a367bac27adbcb1389
Pablo <luis.pablo.lopez.lopez@uabc.edu.mx>
2025-08-15 01:37:32 -0600

    Add swagger

--- apps/tibia-items-api/src/main.ts
+++ apps/tibia-items-api/src/main.ts
@@ -5,6 +5,7 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
@@ -12,6 +13,15 @@ async function bootstrap() {
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
+  const config = new DocumentBuilder()
+    .setTitle('Tibia Items API')
+    .setDescription('A place where you can find the Tibia Items API documentation.')
+    .setVersion('1.0')
+    .addTag('tibia-items')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(port);
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
